7a5432d871795d99330bd5eea3df933e2015d1fc
SenanOrujov <senanorujov04@gmail.com>
2024-03-08 23:21:14 +0400

    first commit

---
+++ Back/SeeN/Helpers/InjectionHelper.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace SeeN.Helpers
+{
+    public static class InjectionHelper
+    {
+        public static void AddAppServices(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            });
+
+            //Auto mapper config 
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            //For loop handling 
+            builder.Services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
+            // Swagger config
+            builder.Services.AddSwaggerGen();
+
+            //Fuluent validation config
+            builder.Services.AddFluentValidationAutoValidation()
+                    .AddFluentValidationClientsideAdapters()
+                    .AddValidatorsFromAssemblyContaining<Program>();
+        }
+    }
+}
---
+++ Back/SeeN/Program.cs
@@ -0,0 +1,40 @@
+
+namespace SeeN
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAppServices(builder);
+
+            var app = builder.Build();
+            
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseAuthentication();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
